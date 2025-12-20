27ba670b4c94b0148be2280115bb3ec10c0c4904
Mohammad Azher Shaikh <m.azhershaikh9767@gmail.com>
2025-12-20 12:47:52 +0530

    Add search functionality to BasicAutomation using Selenium

--- SeleniumAutomation/src/main/java/com/automation/BasicAutomation.java
+++ SeleniumAutomation/src/main/java/com/automation/BasicAutomation.java
@@ -1,13 +1,36 @@
 package com.automation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicAutomation {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.wikipedia.org");
-        System.out.println("Page title is: " + driver.getTitle());
-        driver.quit();
+        // driver.get("https://www.wikipedia.org");
+        // System.out.println("Page title is: " + driver.getTitle());
+        // driver.quit();
+
+        try {
+            // 1. Open Wikipedia
+            driver.get("https://www.wikipedia.org");
+
+            // 2. Locate search input (by ID)
+            WebElement searchBox = driver.findElement(By.id("searchInput"));
+
+            // 3. Enter search text
+            searchBox.sendKeys("Selenium WebDriver");
+
+            // 4. Submit search
+            searchBox.submit();
+
+            // 5. Print result page title
+            System.out.println("Result Page Title: " + driver.getTitle());
+
+        } finally {
+            // 6. Close browser
+            driver.quit();
+        }
     }
 }
